56b601f042303d88980ed1f42060f9dbeccfc665
IamBull8905 <217627756+IamBull8905@users.noreply.github.com>
2026-02-26 19:59:15 +0000

    Dynamically create Todo objects using classes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import "./styles.css";
+
+class CreateToDo {
+    static generateID() {
+        return crypto.randomUUID();
+    };
+
+    constructor(title, description, dueDate, priority, notes, checklist) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+        this.checklist = checklist;
+        this.complete = false;
+        this.id = CreateToDo.generateID();
+    };
+
+    completeTask() {
+        this.complete = true;
+    };
+};
